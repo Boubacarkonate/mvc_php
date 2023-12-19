eb707de234c899923150694a60fe49607473273c
Bouba <boubacar.konate@outlook.fr>
2023-12-19 10:05:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$page_title = "Titre de la page d'accueil"; 
+$page_description = "Description de la page d'accueil"; 
+$page_content = "<h1> PHP MVC </h1>"; 
+
+require_once "views/common/template.php";
